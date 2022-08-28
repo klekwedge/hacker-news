3e6118ee4de3389330b1332d715460fd6e448f8f
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-28 15:34:58 +0300

    feat: add styling for comments

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/self-closing-comp */
 import React, { useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
-import { Box, Heading, Link, List, ListItem } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, Link, List, ListItem } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../hooks/app.hook'
 import { fetchComment, fetchNew } from '../slices/newsSlice'
@@ -57,22 +57,27 @@ function NewsPage() {
           <Heading as='h2' fontWeight='400' fontSize='16px' mb='2px'>
             Comments:
           </Heading>
-          <List display='flex' flexDirection='column' gap='30px'>
+          <List display='flex' flexDirection='column' gap='30px' p='20px 10px'>
             {comments.map((commentItem) => (
-              <ListItem key={uuidv4()}>
-                <Heading
-                  as='h3'
-                  fontWeight='400'
-                  fontSize='16px'
-                  mb='2px'
-                  dangerouslySetInnerHTML={{ __html: commentItem.text }}
-                ></Heading>
-                <Heading as='h2' fontWeight='400' fontSize='16px' mb='2px'>
-                  Author: {commentItem.by}
-                </Heading>
-                <Heading as='h2' fontWeight='400' fontSize='16px' mb='2px'>
-                  {`Time: ${new Date(commentItem.time * 1000).toLocaleString()}`}
-                </Heading>
+              <ListItem key={uuidv4()} display='flex' gap="30px">
+                <Avatar name={commentItem.by} />
+                <Flex flexDirection='column'>
+                  <Flex gap='20px' alignItems='center' mb='10px'>
+                    <Heading as='h2' fontWeight='400' fontSize='16px' mb='2px'>
+                      {commentItem.by}
+                    </Heading>
+                    <Heading as='h2' fontWeight='400' fontSize='16px' mb='2px'>
+                      {`${new Date(commentItem.time * 1000).toLocaleString()}`}
+                    </Heading>
+                  </Flex>
+                  <Heading
+                    as='h3'
+                    fontWeight='400'
+                    fontSize='16px'
+                    mb='2px'
+                    dangerouslySetInnerHTML={{ __html: commentItem.text }}
+                  ></Heading>
+                </Flex>
               </ListItem>
             ))}
           </List>
