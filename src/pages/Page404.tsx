9c579959de0444ef217110b13e188180fbd3a86d
Andrey <antonov.andrey.10101@gmail.com>
2023-08-29 09:44:51 +0400

    feat: add Page404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,5 +1,17 @@
-function Page404() {
-  return <h1>Page404</h1>
+import { Link } from 'react-router-dom';
+import './Page404.scss';
+
+function ErrorPage() {
+  return (
+    <div className="error-page">
+      <div className="error-page__block">
+        <h1>Error 404</h1>
+      </div>
+      <h2>
+        Страница не найдена. <Link to="/">Перейти на главную</Link>
+      </h2>
+    </div>
+  );
 }
 
-export default Page404
+export default ErrorPage;
